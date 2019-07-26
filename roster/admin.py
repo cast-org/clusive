b6bcbd8b2bc274aa817e95c7c10e31167364eb07
Alan Harnum <waharnum@gmail.com>
2019-07-26 16:44:09 -0400

    CSB-507: initial add of period and site models.

--- roster/admin.py
+++ roster/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import ClusiveUser
+from .models import Site, Period, ClusiveUser
 
 class ClusiveUserInline(admin.StackedInline):
     model = ClusiveUser
@@ -12,5 +12,14 @@ class ClusiveUserInline(admin.StackedInline):
 class UserAdmin(BaseUserAdmin):
     inlines = (ClusiveUserInline,)
 
+class PeriodAdminInline(admin.StackedInline):
+    model = Period
+
+class SiteAdmin(admin.ModelAdmin):
+    model = Site
+    inlines = (PeriodAdminInline,)
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Site, SiteAdmin)
+admin.site.register(Period)
--- roster/models.py
+++ roster/models.py
@@ -2,6 +2,28 @@ from django.db import models
 from uuid import uuid4
 from django.contrib.auth.models import User
 
+class Site(models.Model):
+    name = models.CharField(max_length=100)
+    site_id = models.CharField(unique=True, default=uuid4, max_length=36)
+    # TODO: is this an address field? Should it be managed as such?
+    location = models.TextField(max_length=500)
+
+    # TODO: should these three be restricted by choices?
+    language_code = models.CharField(max_length=2)
+    country_code = models.CharField(max_length=2)
+    timezone = models.CharField(max_length=30)
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, self.site_id)
+
+class Period(models.Model):
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    class_id = models.CharField(unique=True, default=uuid4, max_length=36)
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, self.class_id)
+
 class ClusiveUser(models.Model):
     
     # Django standard user class contains the following fields already
@@ -12,11 +34,13 @@ class ClusiveUser(models.Model):
     # - email
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    subject_id = models.CharField(unique=True, default=uuid4, max_length=32)
+    subject_id = models.CharField(unique=True, default=uuid4, max_length=36)
 
     # TODO: should this be an enum of states, as per comment 
     # at https://wiki.cast.org/display/CISL/User+model+development?
     permission = models.BooleanField(default=False)        
+
+    periods = models.ManyToManyField(Period)
 
     # TODO: consider creating a separate "Roles" model to store 
     # roles, and refer to them with a Foreignkey - see comment 
