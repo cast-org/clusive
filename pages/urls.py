16ec1d99025252b29233f609cc2379ff48c4a332
Alan Harnum <waharnum@gmail.com>
2019-08-15 15:20:57 -0400

    CSB-512: basic login/logout, saving before refactoring.

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', LoginView.as_view(
+            template_name='pages/login.html'),
+        name='login'),
+    path('logout', LogoutView.as_view(
+            next_page='index'
+        ), 
+        name='logout'),
 ]
