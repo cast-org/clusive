b64e1158765df0c77ebd699d154e513558a3ae67
Alan Harnum <waharnum@gmail.com>
2019-09-20 12:27:49 -0400

    CSB-579: add a basic autogenerated index page of publications.

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.staticfiles import finders
+from os import listdir
+
+# builds an index of available publications by scanning the 'shared/pubs' directory
+
+def reader_index(request):
+    pubs_directory = finders.find('shared/pubs')
+    pub_directory_names = listdir(pubs_directory)
+    return render(request, 'pages/reader_index.html', context={'pubs': pub_directory_names})
