b44a2866a7743827dfa58c02c325cdfc706cecc9
Boris Goldowsky <bgoldowsky@cast.org>
2019-09-27 09:13:19 -0400

    Import URLs from django_session_timeout So that we can use the view to expire old sessions. Add a little defensive code in logging routine

--- clusive_project/urls.py
+++ clusive_project/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-import django_session_timeout
 
 urlpatterns = [        
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('account/', include('roster.urls')),
-    path('sessions/', include( 'django_session_timeout.urls' )),
+    path('sessions/', include('django_session_timeout.urls')),
 ]
