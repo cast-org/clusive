b8cb5646c46bd6446c182cf9f81f0fed4becc597
Alan Harnum <waharnum@gmail.com>
2019-09-30 16:25:10 -0400

    CSB-518: refactoring to reduce duplication.

--- shared/static/shared/js/clusive-reader-prefs.js
+++ shared/static/shared/js/clusive-reader-prefs.js
@@ -11,8 +11,8 @@
         },
         modelListeners: {
             "preferences.fluid_prefs_textSize": {
-                    func: "{that}.enactReaderTextSize",
-                    args: ["{change}.value"]
+                    func: "cisl.prefs.reader.enactReaderTextSize",
+                    args: ["{change}.value", "{that}.options.settingMaps.textSizeToFontScale"]
             },                      
             "preferences.fluid_prefs_letterSpace": {
                     func: "{that}.enactReaderLetterSpace",
@@ -25,13 +25,21 @@
             "preferences.fluid_prefs_textFont": {
                 func: "{that}.enactReaderTextFont",
                 args: ["{change}.value"]
+            },
+            "preferences.fluid_prefs_contrast": {
+                func: "{that}.enactReaderContrast",
+                args: ["{change}.value"]
             }            
         },
+        settingMaps: {
+            textSizeToFontScale: {
+                "small": 0.75,
+                "medium": 1,
+                "large": 1.5,
+                "x-large": 2                
+            }
+        },
         invokers: {
-            enactReaderTextSize: {
-                funcName: "cisl.prefs.reader.enactReaderTextSize",
-                args: ["{arguments}.0"]
-            },
             enactReaderLetterSpace: {
                 funcName: "cisl.prefs.reader.enactReaderLetterSpace",
                 args: ["{arguments}.0"]                
@@ -43,7 +51,11 @@
             enactReaderTextFont: {
                 funcName: "cisl.prefs.reader.enactReaderTextFont",
                 args: ["{arguments}.0"]                
-            }                        
+            },
+            enactReaderContrast: {
+                funcName: "cisl.prefs.reader.enactReaderContrast",
+                args: ["{arguments}.0"]                       
+            }
         }
     });
 
@@ -55,17 +67,25 @@
         } else return null;
     }
 
-    cisl.prefs.reader.enactReaderTextSize = function (change) {     
+
+    cisl.prefs.reader.setReadiumCSSUserVariable = function (name, value) {
+        var readerHtmlElem = $("#D2Reader-Container").find("iframe").contents().find("html");
+        readerHtmlElem.css(name, value);            
+    }
+
+    cisl.prefs.reader.applyUserSetting = function (settingName, settingValue) {
         var reader = cisl.prefs.reader.getReaderInstance();
-        var fontScaleMap = {
-            "small": "0.75",
-            "medium": "1",
-            "large": "1.5",
-            "x-large": "2"
-        };
         if(reader) {                      
-            reader.applyUserSettings({fontSize:fontScaleMap[change] * 100})
-        }        
+            var settingsObj = 
+            {
+                [settingName]:settingValue
+            };
+            reader.applyUserSettings(settingsObj);
+        }    
+    }
+
+    cisl.prefs.reader.enactReaderTextSize = function (change, settingsMap) {     
+        cisl.prefs.reader.applyUserSetting("fontSize", settingsMap[change] * 100);        
     }
 
     cisl.prefs.reader.enactReaderLetterSpace = function (change) {    
@@ -101,9 +121,19 @@
         }
     }
 
-    cisl.prefs.reader.setReadiumCSSUserVariable = function (name, value) {
-        var readerHtmlElem = $("#D2Reader-Container").find("iframe").contents().find("html");
-        readerHtmlElem.css(name, value);            
+    cisl.prefs.reader.enactReaderContrast = function (change) {
+        var reader = cisl.prefs.reader.getReaderInstance();
+        console.log("enactReaderContrast", change);
+
+        var contrastMap = {
+            "default": "day",
+            "bw": "day",
+            "wb": "night"
+        }
+
+        if(reader) {            
+            reader.applyUserSettings({appearance: contrastMap[change]})            
+        }
     }
 
 })(fluid_3_0_0);
