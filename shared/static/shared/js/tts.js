8278dd264f0e02ab345e871af8fb7c50b74654a8
Alan Harnum <waharnum@gmail.com>
2019-12-18 16:24:04 -0500

    CSB-27: refactor, extract functions.

--- shared/static/shared/js/tts.js
+++ shared/static/shared/js/tts.js
@@ -4,6 +4,49 @@
 
 var clusiveTTS = {};
 
+clusiveTTS.readElements = function (elems) {
+    if(elems.length > 0) {
+        var current = elems.shift();
+        clusiveTTS.readElement(current, elems);            
+    } else {
+        console.log("Done processing");
+    }
+}
+
+clusiveTTS.readElement = function (textElement, remainingElements) {
+    var synth = window.speechSynthesis;        
+    var element = $(textElement);
+    var contentText = element.text();
+    
+    var copiedElement = element.clone(false); 
+    element.after(copiedElement);  
+    element.hide();                     
+    var utterance = new SpeechSynthesisUtterance(contentText);
+    
+    utterance.onboundary = function (e) {
+        if(e.name === "sentence") {
+            console.log("sentence boundary", e.charIndex, e.charLength, contentText.slice(e.charIndex, e.charIndex + e.charLength));                                                
+        }
+        if(e.name === "word") {
+            console.log("word boundary", e.charIndex, e.charLength, contentText.slice(e.charIndex, e.charIndex + e.charLength));
+
+            var preceding = contentText.substring(0, e.charIndex);
+            var middle = contentText.substring(e.charIndex, e.charIndex+e.charLength);
+            var following = contentText.substring(e.charIndex+e.charLength)
+            var newText = preceding + "<span class='tts-currentWord'>" + middle + "</span>" + following;
+            copiedElement.html(newText);
+        }
+    }
+
+    utterance.onend = function (e) {      
+        copiedElement.remove();
+        element.show();                  
+        process(remainingElements);
+    }
+
+    synth.speak(utterance);
+}
+
 clusiveTTS.start = function() {
     console.log("tts loaded");
     
@@ -18,50 +61,7 @@ clusiveTTS.start = function() {
         textElements.push(e);
     });
 
-    var process = function (te) {                
-        if(te.length > 0) {
-            var current = te.shift();
-            speak(current, te);            
-        } else {
-            console.log("Done processing");
-        }
-    }
-
-    var speak = function (textElement, remainingElements) {
-        var synth = window.speechSynthesis;        
-        var element = $(textElement);
-        var contentText = element.text();
-        
-        var copiedElement = element.clone(false); 
-        element.after(copiedElement);  
-        element.hide();                     
-        var utterance = new SpeechSynthesisUtterance(contentText);
-        
-        utterance.onboundary = function (e) {
-            if(e.name === "sentence") {
-                console.log("sentence boundary", e.charIndex, e.charLength, contentText.slice(e.charIndex, e.charIndex + e.charLength));                                                
-            }
-            if(e.name === "word") {
-                console.log("word boundary", e.charIndex, e.charLength, contentText.slice(e.charIndex, e.charIndex + e.charLength));
-
-                var preceding = contentText.substring(0, e.charIndex);
-                var middle = contentText.substring(e.charIndex, e.charIndex+e.charLength);
-                var following = contentText.substring(e.charIndex+e.charLength)
-                var newText = preceding + "<span class='tts-currentWord'>" + middle + "</span>" + following;
-                copiedElement.html(newText);
-            }
-        }
-
-        utterance.onend = function (e) {      
-            copiedElement.remove();
-            element.show();                  
-            process(remainingElements);
-        }
-
-        synth.speak(utterance);
-    }
-
-    process(textElements);
+    clusiveTTS.readElements(textElements);
 
 };
 
