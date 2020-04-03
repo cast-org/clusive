b6a304e3e8819d5e8f29fbd42a14d7d7db2f4425
Boris Goldowsky <bgoldowsky@cast.org>
2020-04-03 11:45:27 -0400

    CSB-872: Mostly working TOC. Waiting on final design with active state.

--- src/frontend/js/frontend.js
+++ src/frontend/js/frontend.js
@@ -50,6 +50,84 @@ function isImageLoaded($img, instance, callback) {
     proxyImg.src = img.src;
 }
 
+function table_of_contents_level(list, level, id) {
+    'use strict';
+
+    var out = '<ul class="nav nav-vertical">';
+    list.forEach(function(element, index) {
+        var toc_depth = 'toc-depth-' + level;
+        var li_class = 'nav-link ' + toc_depth;
+        // eslint-disable-next-line no-script-url
+        var click = 'javascript:D2Reader.goTo({\'href\':\'' + element.href + '\'}); return false;';
+        var twiddle = '';
+        var submenu = '';
+        var submenu_id = id + '_' + index;
+        if (element.children) {
+            twiddle = '<a href="#' +
+                submenu_id +
+                '" class="has-children ' +
+                toc_depth +
+                '" data-cfw="collapse" data-cfw-collapse-animate="false">' +
+                '<span class="icon-angle-right" aria-hidden="true"></span>\n' +
+                '<span class="sr-only">Toggle menu for item ' +
+                element.title +
+                '</span></a>';
+
+            submenu = '<div id="' +
+                submenu_id +
+                '" class="collapse">' +
+                table_of_contents_level(element.children, level + 1, submenu_id) +
+                '</div>';
+        }
+        out += '<li class="nav-item">' +
+            twiddle +
+            '<a href="' +
+            element.href +
+            '" onclick="' + click + '" class="' + li_class + '">' +
+            element.title +
+            '</a>' +
+            submenu +
+            '</li>';
+    });
+    out += '</ul>';
+
+    return out;
+}
+
+// eslint-disable-next-line no-undef,no-unused-vars
+function build_table_of_contents() {
+    'use strict';
+
+    // eslint-disable-next-line no-undef
+    if (typeof D2Reader === 'object') {
+        // eslint-disable-next-line no-undef
+        D2Reader.tableOfContents().then(function(x) {
+            var out = table_of_contents_level(x, 0, 'toc');
+            $('#contents_list').html(out).CFW_Init();
+        });
+    }
+}
+
+// eslint-disable-next-line no-unused-vars
+function highlight_current_toc_item() {
+    'use strict';
+
+    // eslint-disable-next-line no-undef
+    var current = D2Reader.mostRecentNavigatedTocItem();
+    if (current.startsWith('/')) {
+        current = current.substr(1);
+    }
+    var top = $('#contents_list');
+    var elt = top.find('a[href$=\'' + current + '\']');
+    // Add active class to just the selected element.
+    top.find('a.active').removeClass('active');
+    elt.addClass('active');
+    // Open collapsers to show the current section
+    elt.parents('li').children('a[data-cfw="collapse"]').CFW_Collapse('show');
+    // FIXME - for some reason this is not working
+    elt[0].scrollIntoView();
+}
+
 $(window).ready(function() {
     'use strict';
 
