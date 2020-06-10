cccfe2e56aaed13372efaa75d7f16a0da7dedcde
Boris Goldowsky <bgoldowsky@cast.org>
2020-06-10 11:50:59 -0400

    CSL-260: design tweaks to password pages.
    Added new filter to help with formatting form labels figuration-style.

--- src/pages/templatetags/figuration.py
+++ src/pages/templatetags/figuration.py
@@ -14,12 +14,31 @@ def formcontrol(value):
     This gives it the Figuration look and feel.
     """
 
-    match = re.search(r'<input.*>', value)
+    match = re.search(r'<input.*?>', value)
     input = match.group()
     classmatch = re.search(r'class="', input)
     if classmatch:
         newinput = input[:classmatch.start()] + 'class="form-control ' + input[classmatch.end():]
     else:
         newinput = input[:-1] + 'class="form-control">'
+
+    return value[:match.start()] + newinput + value[match.end():]
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def formlabel(value):
+    """
+    Adds the form-label class attribute to the first <label> found.
+    This gives it the Figuration look and feel.
+    """
+
+    match = re.search(r'<label.*?>', value)
+    input = match.group()
+    classmatch = re.search(r'class="', input)
+    if classmatch:
+        newinput = input[:classmatch.start()] + 'class="form-label ' + input[classmatch.end():]
+    else:
+        newinput = input[:-1] + 'class="form-label">'
 
     return value[:match.start()] + newinput + value[match.end():]
