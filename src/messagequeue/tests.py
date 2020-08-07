1346a0bc5b0ce7e7150d208f9f786f4feda8fd4c
Alan Harnum <waharnum@gmail.com>
2020-08-07 12:16:20 -0400

    CSL-404: add test for confirming preference changes.

--- src/messagequeue/tests.py
+++ src/messagequeue/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
+import json
 
 from roster.tests import set_up_test_users
 
 from .models import Message, client_side_prefs_change
 
-# Create your tests here.
+preference_change_message_queue = '{"timestamp":"2020-07-28T21:01:29.448Z","messages":[{"content":{"type":"PC","preferences":{"fluid_prefs_textFont":"comic","textFont":"comic","fluid_prefs_textSize":0.9,"textSize":0.9}},"timestamp":"2020-07-28T21:00:26.602Z"},{"content":{"type":"PC","preferences":{"fluid_prefs_textFont":"times","textFont":"times","fluid_prefs_textSize":0.9,"textSize":0.9}},"timestamp":"2020-07-28T21:00:30.134Z"},{"content":{"type":"PC","preferences":{"fluid_prefs_contrast":"sepia","theme":"sepia","fluid_prefs_textFont":"times","textFont":"times","fluid_prefs_textSize":0.9,"textSize":0.9}},"timestamp":"2020-07-28T21:00:32.938Z"}]}'    
 
 class MessageQueueTestCase(TestCase):
 
@@ -14,26 +15,31 @@ class MessageQueueTestCase(TestCase):
     def setUp(self):
         set_up_test_users()
 
-    def test_send_message(self):        
-        
-        preference_change_json = '{"timestamp":"2020-07-28T21:01:29.448Z","messages":[{"content":{"type":"PC","preferences":{"fluid_prefs_textFont":"comic","textFont":"comic","fluid_prefs_textSize":0.9,"textSize":0.9}},"timestamp":"2020-07-28T21:00:26.602Z"},{"content":{"type":"PC","preferences":{"fluid_prefs_textFont":"times","textFont":"times","fluid_prefs_textSize":0.9,"textSize":0.9}},"timestamp":"2020-07-28T21:00:30.134Z"},{"content":{"type":"PC","preferences":{"fluid_prefs_contrast":"sepia","theme":"sepia","fluid_prefs_textFont":"times","textFont":"times","fluid_prefs_textSize":0.9,"textSize":0.9}},"timestamp":"2020-07-28T21:00:32.938Z"}]}'
-    
+    def test_send_message(self):                            
         login = self.client.login(username='user1', password='password1')
 
-        response = self.client.post('/messagequeue/', preference_change_json, content_type='application/json')
+        response = self.client.post('/messagequeue/', preference_change_message_queue, content_type='application/json')
         self.assertJSONEqual(response.content, {'success': 1}, 'Sending messages did not return expected response')
 
-    def test_send_message_signal(self):
-        preference_change_json = '{"timestamp":"2020-07-28T21:01:29.448Z","messages":[{"content":{"type":"PC","preferences":{"fluid_prefs_textFont":"comic","textFont":"comic","fluid_prefs_textSize":0.9,"textSize":0.9}},"timestamp":"2020-07-28T21:00:26.602Z"},{"content":{"type":"PC","preferences":{"fluid_prefs_textFont":"times","textFont":"times","fluid_prefs_textSize":0.9,"textSize":0.9}},"timestamp":"2020-07-28T21:00:30.134Z"},{"content":{"type":"PC","preferences":{"fluid_prefs_contrast":"sepia","theme":"sepia","fluid_prefs_textFont":"times","textFont":"times","fluid_prefs_textSize":0.9,"textSize":0.9}},"timestamp":"2020-07-28T21:00:32.938Z"}]}'
-    
+    def test_client_side_prefs_change_message(self):            
+        login = self.client.login(username='user1', password='password1')
+
+        self.client.post('/messagequeue/', preference_change_message_queue, content_type='application/json')
+        prefs_response = self.client.get('/account/prefs')
+        prefs = json.loads(prefs_response.content)
+        self.assertEqual(prefs["fluid_prefs_contrast"], "sepia", "fluids_prefs_contrast value was not as expected")        
+        self.assertEqual(prefs["fluid_prefs_textFont"], "times", "fluid_prefs_textFont value was not as expected")        
+
+    def test_send_message_signal(self):            
         login = self.client.login(username='user1', password='password1')
 
         # Following pattern for testing signals described at https://www.freecodecamp.org/news/how-to-testing-django-signals-like-a-pro-c7ed74279311/
         self.signal_was_called = False;
         def handler(sender, timestamp, content, request, **kwargs):
+            print(sender, timestamp, content, request)
             self.signal_was_called = True;
+
         client_side_prefs_change.connect(handler)
 
-        response = self.client.post('/messagequeue/', preference_change_json, content_type='application/json')        
-        self.assertJSONEqual(response.content, {'success': 1}, 'Sending messages did not return expected response')
+        response = self.client.post('/messagequeue/', preference_change_message_queue, content_type='application/json')                
         self.assertTrue(self.signal_was_called)
