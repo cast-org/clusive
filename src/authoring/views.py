a171189cbc0e3519eb046b06bc51780df347afe5
Boris Goldowsky <bgoldowsky@cast.org>
2020-08-17 15:29:37 -0400

    Add synonym lookup column.

--- src/authoring/views.py
+++ src/authoring/views.py
@@ -1,7 +1,9 @@
 import logging
 
-from django.views.generic import FormView
+from django.views.generic import FormView, TemplateView
 import wordfreq as wf
+from nltk.corpus import wordnet
+from nltk.corpus.reader import Synset
 from textstat import textstat
 
 from authoring.forms import TextInputForm
@@ -14,14 +16,12 @@ class LevelingView(FormView):
     template_name = 'authoring/level.html'
     form_class = TextInputForm
     lang = 'en'
-    word_count = 0
     words = []
-    stats = {}
+    stats = []
 
     def form_valid(self, form):
         text = form.cleaned_data['text']
         word_list = wf.tokenize(text, self.lang)
-        self.word_count = len(word_list)
         self.stats = [
             { 'name': 'Flesch-Kincaid grade level',
               'value':  textstat.flesch_kincaid_grade(text),
@@ -29,6 +29,8 @@ class LevelingView(FormView):
             { 'name': 'Dale-Chall grade level',
               'value': textstat.dale_chall_readability_score_v2(text),
               'desc': 'Based on avg sentence length and percent difficult words.'},
+            { 'name': 'Number of words',
+              'value': textstat.lexicon_count(text) },
             { 'name': 'Number of sentences',
               'value': textstat.sentence_count(text) },
             { 'name': 'Average sentence length',
@@ -38,7 +40,7 @@ class LevelingView(FormView):
             { 'name': 'Difficult words',
               'value': "%d (%d%%): %s" % (textstat.difficult_words(text),
                                           100*textstat.difficult_words(text)/textstat.lexicon_count(text),
-                                          textstat.difficult_words_list(text)) },
+                                          ', '.join(textstat.difficult_words_list(text))) },
         ]
         word_info = {}
         for word in word_list:
@@ -47,7 +49,7 @@ class LevelingView(FormView):
             if w:
                 w['count'] += 1
                 if word != base and word not in w['alts']:
-                    w['forms'].append(word)
+                    w['alts'].append(word)
             else:
                 w = {
                     'hw' : base,
@@ -69,9 +71,20 @@ class LevelingView(FormView):
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        data['word_count'] = self.word_count
         data['words'] = self.words
         data['stats'] = self.stats
         return data
 
 
+class SynonymsView(TemplateView):
+    template_name = 'authoring/partial/synonyms.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        synsets = wordnet.synsets(kwargs['word'])
+        context['synonyms'] = [ describe_synset(synset) for synset in synsets ]
+        return context
+
+
+def describe_synset(synset : Synset):
+    return "(%s) %s (%s)" % (synset.pos(), ', '.join([str(lemma.name()) for lemma in synset.lemmas()]), synset.definition())
