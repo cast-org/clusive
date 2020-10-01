8da6d6ac4860d31bc4d054864f8c44c7d312148b
Matthew Brambilla <mbrambilla@cast.org>
2020-10-01 16:37:27 -0400

    CSL-309: Add test for mockups

--- src/frontend/js/theme-test.js
+++ src/frontend/js/theme-test.js
@@ -3,10 +3,16 @@
 function themeControls() {
     'use strict';
 
-    var html = '<div style="position: fixed; bottom: .25rem; left: 50%; z-index: 2000; transform: translateX(-50%); background: #ddd; padding: 0 .5rem; border: 2px solid #666;">';
+    var html = '<div style="position: fixed; bottom: .25rem; left: 50%; z-index: 2000; transform: translateX(-50%); background: #ddd; padding: 0 .5rem; border: 2px solid #666; text-align: center; font-size: .875rem;">';
+    html += 'Theme: ';
     html += '<a href="#" style="color: #009 !important;" onclick="return themeCSS();">Default</a> |';
     html += '<a href="#" style="color: #009 !important;" onclick="return themeCSS(\'sepia\');">Sepia</a> |';
     html += '<a href="#" style="color: #009 !important;" onclick="return themeCSS(\'night\');">Night</a> ';
+    html += '</br>';
+    html += 'Line height: ';
+    html += '<a href="#" style="color: #009 !important;" onclick="return themeLH(1.2);">Short</a> |';
+    html += '<a href="#" style="color: #009 !important;" onclick="return themeLH(1.6);">Default</a> |';
+    html += '<a href="#" style="color: #009 !important;" onclick="return themeLH(2);">Tall</a>';
     html += '</div>';
     $(document.body).append(html);
 }
@@ -27,8 +33,21 @@ function themeCSS(name) {
     return false;
 }
 
+function themeLH(dim) {
+    'use strict';
+
+    if (typeof dim === 'undefined') {
+        dim = 1.6;
+    }
+    document.body.style.lineHeight = dim;
+    document.body.style.setProperty('--CT_lineHeight', dim);
+
+    return false;
+}
+
 $(window).ready(function() {
     'use strict';
 
     themeControls();
+    themeLH();
 });
