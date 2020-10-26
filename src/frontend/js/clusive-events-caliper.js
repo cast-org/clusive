0de6b82fb7a9b8eafdafdf79815e73fd0131715f
Alan Harnum <waharnum@gmail.com>
2020-10-26 15:50:26 -0400

    CSL-485: add generation of research events via annotation of markup with data-clusive-event attribute.

--- src/frontend/js/clusive-events-caliper.js
+++ src/frontend/js/clusive-events-caliper.js
@@ -6,6 +6,7 @@ var caliperEventTypes = {
     TOOL_USE_EVENT: "TOOL_USE_EVENT"
 }
 
+// Define explicitly tracked control interactions here
 var trackedControlInteractions = [
     {
         selector: ".btn.tts-play",
@@ -42,6 +43,29 @@ var addControlInteractionToQueue = function (control, value) {
 };
 
 $(document).ready(function () {    
+
+    // Build additional control interaction objects here from any data-clusive-event attributes on markup
+    // data-clusive-event="[handler]|[control]|[value]"
+    // example: data-clusive-event="click|settings-sidebar|opened"
+
+    // ="click|settings-sidebar|opened"
+    $("*[data-clusive-event").each(function (i, control) {
+        // data-clusive-event attribute 
+        console.debug("data-clusive-event", control)
+        var defsValsFromAttr = $(control).attr("data-clusive-event").split("|");
+        if(defsValsFromAttr.length < 3) {
+            console.debug("invalid data-clusive-event attribute value on control", control);
+        }
+        var interactionDef = {
+            selector: control,
+            handler: defsValsFromAttr[0],
+            control: defsValsFromAttr[1],
+            value: defsValsFromAttr[2]
+        }
+        trackedControlInteractions.push(interactionDef);
+    });
+
+    // Set up events control interactions here
     trackedControlInteractions.forEach(function (interactionDef, i) {              
         var control = $(interactionDef.selector);
         console.debug("Event for control tracking: ", interactionDef, control)      
