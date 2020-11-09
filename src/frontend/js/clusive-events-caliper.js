f0f1664e5a048ad8a91e6f9ed79c79a7b7c2e04c
Boris Goldowsky <bgoldowsky@cast.org>
2020-11-09 11:00:06 -0500

    Fix many eslint warnings in file

--- src/frontend/js/clusive-events-caliper.js
+++ src/frontend/js/clusive-events-caliper.js
@@ -1,77 +1,84 @@
-'use strict'
+/* global PAGE_EVENT_ID, clusive */
 
-var addControlInteractionToQueue = function (control, value) {    
-    console.debug("Adding control interaction to queue: ", control, value)
+var addControlInteractionToQueue = function(control, value) {
+    'use strict';
+
+    console.debug('Adding control interaction to queue: ', control, value);
     clusiveEvents.messageQueue.add({
-        "type": "CE", 
-        "caliperEvent": {"type": clusiveEvents.caliperEventTypes.TOOL_USE_EVENT, "control": control, "value": value},
-        "readerInfo": clusiveContext.reader.info,
-        "eventId": PAGE_EVENT_ID
+        type: 'CE',
+        caliperEvent: {
+            type: clusiveEvents.caliperEventTypes.TOOL_USE_EVENT,
+            control: control,
+            value: value
+        },
+        readerInfo: clusiveContext.reader.info,
+        eventId: PAGE_EVENT_ID
     });
 };
 
 var clusiveEvents = {
-    messageQueue: clusive.djangoMessageQueue(),    
+    messageQueue: clusive.djangoMessageQueue(),
     caliperEventTypes: {
-        TOOL_USE_EVENT: "TOOL_USE_EVENT"
-    },    
+        TOOL_USE_EVENT: 'TOOL_USE_EVENT'
+    },
     trackedControlInteractions: [
         {
-            selector: ".btn.tts-play",
-            handler: "click",
-            control: "tts-play",
-            value: "clicked"
+            selector: '.btn.tts-play',
+            handler: 'click',
+            control: 'tts-play',
+            value: 'clicked'
         },
         {
-            selector: ".btn.tts-pause",
-            handler: "click",
-            control: "tts-pause",
-            value: "clicked"
+            selector: '.btn.tts-pause',
+            handler: 'click',
+            control: 'tts-pause',
+            value: 'clicked'
         },
         {
-            selector: ".btn.tts-resume",
-            handler: "click",
-            control: "tts-resume",
-            value: "clicked"
+            selector: '.btn.tts-resume',
+            handler: 'click',
+            control: 'tts-resume',
+            value: 'clicked'
         },
         {
-            selector: ".btn.tts-stop",
-            handler: "click",
-            control: "tts-stop",
-            value: "clicked"
-        }         
+            selector: '.btn.tts-stop',
+            handler: 'click',
+            control: 'tts-stop',
+            value: 'clicked'
+        }
     ],
-    addControlInteractionToQueue: addControlInteractionToQueue    
-}
+    addControlInteractionToQueue: addControlInteractionToQueue
+};
 
-$(document).ready(function () {    
+$(document).ready(function() {
+    'use strict';
 
     // Build additional control interaction objects here from any data-clusive-event attributes on page markup
     // synax: data-clusive-event="[handler]|[control]|[value]"
-    // example: data-clusive-event="click|settings-sidebar|opened"    
-    $("*[data-clusive-event").each(function (i, control) {
-        // data-clusive-event attribute 
-        console.debug("data-clusive-event", control)
-        var defsValsFromAttr = $(control).attr("data-clusive-event").split("|");
-        if(defsValsFromAttr.length < 3) {
-            console.debug("invalid data-clusive-event attribute value on control", control);
+    // example: data-clusive-event="click|settings-sidebar|opened"
+    $('*[data-clusive-event]').each(function(i, control) {
+        // data-clusive-event attribute
+        console.debug('data-clusive-event', control);
+        var defsValsFromAttr = $(control).attr('data-clusive-event').split('|');
+        if (defsValsFromAttr.length < 3) {
+            console.debug('invalid data-clusive-event attribute value on control', control);
         }
         var interactionDef = {
             selector: control,
             handler: defsValsFromAttr[0],
             control: defsValsFromAttr[1],
             value: defsValsFromAttr[2]
-        }
+        };
         clusiveEvents.trackedControlInteractions.push(interactionDef);
     });
 
     // Set up events control interactions here
-    clusiveEvents.trackedControlInteractions.forEach(function (interactionDef, i) {              
+    clusiveEvents.trackedControlInteractions.forEach(function(interactionDef) {
         var control = $(interactionDef.selector);
-        console.debug("Event for control tracking: ", interactionDef, control)      
+        console.debug('Event for control tracking: ', interactionDef, control);
         var handler = interactionDef.handler;
-        $(interactionDef.selector)[handler](function () {                    
+        $(interactionDef.selector)[handler](function() {
             addControlInteractionToQueue(interactionDef.control, interactionDef.value);
-        })
-    });            
-})
+        });
+    });
+});
