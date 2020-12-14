a4e56b5551fd0693e38b57a09c0fd9167af06b48
Alan Harnum <waharnum@gmail.com>
2020-12-14 10:53:42 -0500

    NOJIRA: fix a very dumb bug with control/value string formatting; add a 'SKIPPED' event for assessment that originally surfaced it.

--- src/frontend/js/glossary.js
+++ src/frontend/js/glossary.js
@@ -185,6 +185,7 @@ vocabCheck.selected = function(value) {
 };
 
 vocabCheck.skip = function() {
+    clusiveEvents.addVocabCheckSkippedEventToQueue();
     window.location = '/reader/' + vocabCheck.pendingArticle;
     return false;
 };
--- src/messagequeue/models.py
+++ src/messagequeue/models.py
@@ -43,11 +43,12 @@ class Message:
 
     def send_client_side_caliper_event(self):    
         event_id = self.content['eventId']  
-        control = self.content["caliperEvent"]["control"]
-        value = self.content["caliperEvent"]["value"]   
-        reader_info = self.content["readerInfo"]     
+        event_type = self.content['caliperEvent']['type']
+        control = self.content['caliperEvent']['control']
+        value = self.content['caliperEvent']['value']
+        reader_info = self.content['readerInfo']
         control_used.send(sender=self.__class__, timestamp=self.timestamp,
-                          request=self.request, event_id = event_id, control=control, value=value, reader_info=reader_info)
+                          request=self.request, event_id = event_id, type=event_type, control=control, value=value, reader_info=reader_info)
 
     def send_page_timing(self):
         event_id = self.content['eventId']
