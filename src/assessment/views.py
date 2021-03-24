16dd10ab53321edc340e9d6ad49ade5c7d477f85
Alan Harnum <waharnum@gmail.com>
2021-03-24 16:25:37 -0400

    CSL-864: basic create working, need to link up related page event.

--- src/assessment/views.py
+++ src/assessment/views.py
@@ -4,6 +4,12 @@ from django.http import JsonResponse
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from roster.models import ClusiveUser
+from library.models import BookVersion
+
+from .models import ComprehensionCheckResponse
+
+
 import json
 
 logger = logging.getLogger(__name__)
@@ -11,10 +17,22 @@ logger = logging.getLogger(__name__)
 class ComprehensionCheckView(LoginRequiredMixin, View):
     def post(self, request):            
         try:
-            comprehension_check_response = json.loads(request.body)            
-            logger.info('Received a valid comprehension check response: %s' % comprehension_check_response)
+            comprehension_check_data = json.loads(request.body)            
+            logger.info('Received a valid comprehension check response: %s' % comprehension_check_data)
         except json.JSONDecodeError:
             logger.warning('Received malformed comprehension check data: %s' % request.body)
             return JsonResponse(status=501, data={'message': 'Invalid JSON in request body'})
-        # Fill in user id and save            
-        return JsonResponse({"foo": "bar"})        
+        
+        clusive_user = ClusiveUser.objects.get(user=self.request.user)
+        book_version = BookVersion.objects.get(id=comprehension_check_data.get("bookVersionId"))
+
+        comprehension_check_response = ComprehensionCheckResponse(
+            user = clusive_user,
+            book_version = book_version,
+            comprehension_scale_response = comprehension_check_data.get("scaleResponse"),
+            comprehension_free_response = comprehension_check_data.get("freeResponse")
+        )
+
+        comprehension_check_response.save()
+
+        return JsonResponse({"success": "1"})        
