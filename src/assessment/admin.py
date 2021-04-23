6e82cffa8adbd3fa74d236092c2a598a05a316ef
Alan Harnum <waharnum@gmail.com>
2021-04-23 13:22:57 -0400

    CSL-834: add affective check responses to admin interface.

--- src/assessment/admin.py
+++ src/assessment/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from assessment.models import ComprehensionCheckResponse
+from assessment.models import AffectiveCheckResponse, ComprehensionCheckResponse
 
 admin.site.register(ComprehensionCheckResponse)
+admin.site.register(AffectiveCheckResponse)
