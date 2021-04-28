43cbdb438c239d8e70416c98db1c017df471eb24
Boris Goldowsky <bgoldowsky@cast.org>
2021-04-28 09:54:10 -0400

    CSL-834: admin site display customization for assessments.

--- src/assessment/admin.py
+++ src/assessment/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from assessment.models import AffectiveCheckResponse, ComprehensionCheckResponse
 
-admin.site.register(ComprehensionCheckResponse)
-admin.site.register(AffectiveCheckResponse)
+
+@admin.register(AffectiveCheckResponse)
+class AffectiveCheckResponseAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'created', 'updated')
+    list_display = ('updated', 'user', 'book')
+    ordering = ('-updated',)
+
+
+@admin.register(ComprehensionCheckResponse)
+class ComprehensionCheckResponseAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', 'created', 'updated')
+    list_display = ('updated', 'user', 'book', 'comprehension_scale_response')
+    ordering = ('-updated',)
