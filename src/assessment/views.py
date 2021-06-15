7e6a147e4ddd6746d697a90ac9fa3150b8a660eb
Boris Goldowsky <bgoldowsky@cast.org>
2021-06-15 12:59:19 -0400

    CSL-966: Continued work on affect dashboard panel and related functionality. Refactor dashboard page setup so each panel has its own visibility flag and data. Add AffectiveBookTotal model.

--- src/assessment/views.py
+++ src/assessment/views.py
@@ -2,13 +2,15 @@ import json
 import logging
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import transaction
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views import View
 
 from eventlog.signals import affect_check_completed, comprehension_check_completed
 from library.models import Book
-from .models import ComprehensionCheck, ComprehensionCheckResponse, AffectiveCheck, AffectiveCheckResponse
+from .models import ComprehensionCheck, ComprehensionCheckResponse, AffectiveCheck, AffectiveCheckResponse, \
+    AffectiveUserTotal, AffectiveBookTotal
 
 logger = logging.getLogger(__name__)
 
@@ -17,48 +19,56 @@ class AffectCheckView(LoginRequiredMixin, View):
     def create_from_request(request, affect_check_data, book_id):
         clusive_user = request.clusive_user
         logger.debug("create with data: %s", affect_check_data)
-        logger.debug("create with data_bookId: %s", affect_check_data.get("bookId"))
-        book = Book.objects.get(id=book_id)            
+        book = Book.objects.get(id=book_id)
 
-        (acr, created) = AffectiveCheckResponse.objects.get_or_create(user=clusive_user, book=book)
-        acr.annoyed_option_response = affect_check_data.get('affect-option-annoyed')
-        acr.bored_option_response = affect_check_data.get('affect-option-bored')
-        acr.calm_option_response = affect_check_data.get('affect-option-calm')
-        acr.confused_option_response = affect_check_data.get('affect-option-confused')
-        acr.curious_option_response = affect_check_data.get('affect-option-curious')
-        acr.disappointed_option_response = affect_check_data.get('affect-option-disappointed')
-        acr.frustrated_option_response = affect_check_data.get('affect-option-frustrated')
-        acr.happy_option_response = affect_check_data.get('affect-option-happy')
-        acr.interested_option_response = affect_check_data.get('affect-option-interested')
-        acr.okay_option_response = affect_check_data.get('affect-option-okay')
-        acr.sad_option_response = affect_check_data.get('affect-option-sad')
-        acr.surprised_option_response = affect_check_data.get('affect-option-surprised')
+        with transaction.atomic():
+            (acr, created) = AffectiveCheckResponse.objects.get_or_create(user=clusive_user, book=book)
+            orig_values = acr.to_list()
+            acr.annoyed_option_response = affect_check_data.get('affect-option-annoyed')
+            acr.bored_option_response = affect_check_data.get('affect-option-bored')
+            acr.calm_option_response = affect_check_data.get('affect-option-calm')
+            acr.confused_option_response = affect_check_data.get('affect-option-confused')
+            acr.curious_option_response = affect_check_data.get('affect-option-curious')
+            acr.disappointed_option_response = affect_check_data.get('affect-option-disappointed')
+            acr.frustrated_option_response = affect_check_data.get('affect-option-frustrated')
+            acr.happy_option_response = affect_check_data.get('affect-option-happy')
+            acr.interested_option_response = affect_check_data.get('affect-option-interested')
+            acr.okay_option_response = affect_check_data.get('affect-option-okay')
+            acr.sad_option_response = affect_check_data.get('affect-option-sad')
+            acr.surprised_option_response = affect_check_data.get('affect-option-surprised')
+            acr.save()
 
-        acr.save()
+            new_values = acr.to_list()
+            (aut, created) = AffectiveUserTotal.objects.get_or_create(user=clusive_user)
+            aut.update(orig_values, new_values)
+            aut.save()
+
+            (abt, created) = AffectiveBookTotal.objects.get_or_create(book=book)
+            abt.update(orig_values, new_values)
+            abt.save()
 
         page_event_id=affect_check_data.get("eventId")
-        
         affect_check_completed.send(sender=AffectCheckView,
                                   request=request, event_id=page_event_id,
-                                  affect_check_response_id=acr.id,                                  
-                                  answer=acr.toAnswerString())
-        
+                                  affect_check_response_id=acr.id,
+                                  answer=acr.to_answer_string())
+
     def post(self, request, book_id):
         try:
             affect_check_data = json.loads(request.body)
             logger.info('Received a valid affect check response: %s' % affect_check_data)
         except json.JSONDecodeError:
             logger.warning('Received malformed affect check data: %s' % request.body)
-            return JsonResponse(status=501, data={'message': 'Invalid JSON in request body'}) 
+            return JsonResponse(status=501, data={'message': 'Invalid JSON in request body'})
 
-        AffectCheckView.create_from_request(request, affect_check_data, book_id)                
+        AffectCheckView.create_from_request(request, affect_check_data, book_id)
 
         return JsonResponse({"success": "1"})
 
     def get(self, request, book_id):
         user = request.clusive_user
-        book = Book.objects.get(id=book_id)        
-        acr = get_object_or_404(AffectiveCheckResponse, user=user, book=book)            
+        book = Book.objects.get(id=book_id)
+        acr = get_object_or_404(AffectiveCheckResponse, user=user, book=book)
         response_value = {
             "affect-option-annoyed": acr.annoyed_option_response,
             "affect-option-bored": acr.bored_option_response,
@@ -73,11 +83,11 @@ class AffectCheckView(LoginRequiredMixin, View):
             "affect-option-sad": acr.sad_option_response,
             "affect-option-surprised": acr.surprised_option_response,
         }
-        return JsonResponse(response_value)        
+        return JsonResponse(response_value)
 
 class ComprehensionCheckView(LoginRequiredMixin, View):
     @staticmethod
-    def create_from_request(request, comprehension_check_data, book_id):    
+    def create_from_request(request, comprehension_check_data, book_id):
         clusive_user = request.clusive_user
         book = Book.objects.get(id=book_id)
 
@@ -100,24 +110,24 @@ class ComprehensionCheckView(LoginRequiredMixin, View):
                                 comprehension_check_response_id=ccr.id,
                                 key=ComprehensionCheck.free_response_key,
                                 question=comprehension_check_data.get('freeQuestion'),
-                                answer=ccr.comprehension_free_response)            
+                                answer=ccr.comprehension_free_response)
 
-    def post(self, request, book_id):            
+    def post(self, request, book_id):
         try:
-            comprehension_check_data = json.loads(request.body)            
+            comprehension_check_data = json.loads(request.body)
             logger.info('Received a valid comprehension check response: %s' % comprehension_check_data)
         except json.JSONDecodeError:
             logger.warning('Received malformed comprehension check data: %s' % request.body)
             return JsonResponse(status=501, data={'message': 'Invalid JSON in request body'})
 
         ComprehensionCheckView.create_from_request(request, comprehension_check_data, book_id)
-                
+
         return JsonResponse({"success": "1"})
 
     def get(self, request, book_id):
         user = request.clusive_user
-        book = Book.objects.get(id=book_id)        
-        ccr = get_object_or_404(ComprehensionCheckResponse, user=user, book=book)            
+        book = Book.objects.get(id=book_id)
+        ccr = get_object_or_404(ComprehensionCheckResponse, user=user, book=book)
         response_value = {ComprehensionCheck.scale_response_key: ccr.comprehension_scale_response,
                        ComprehensionCheck.free_response_key: ccr.comprehension_free_response}
         return JsonResponse(response_value)
