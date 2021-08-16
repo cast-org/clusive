f459447f5438f9bf0906172ebf7cd13d54a8dbdd
Boris Goldowsky <bgoldowsky@cast.org>
2021-08-16 10:48:03 -0400

    CSL-728: load language list from API, filtered to 30 most common ELL home languages.

--- src/translation/views.py
+++ src/translation/views.py
@@ -26,7 +26,7 @@ class TranslateTextView(LoginRequiredMixin, View):
         if not lang or lang == 'default':
             result = 'What language do you want to translate to? Choose one in Settings under Reading Tools'
         else:
-            client = GoogleApiManager.get_google_translate_client()
+            client = TranslateApiManager.get_google_translate_client()
             if client:
                 try:
                     # TODO: set source language?  Currently lets Google auto-detect.
@@ -39,19 +39,36 @@ class TranslateTextView(LoginRequiredMixin, View):
                 result = 'Sorry, translation feature is unavailable'
 
         return JsonResponse({'result': result,
-                             'direction': self.direction_for_language(lang)})
+                             'direction': TranslateApiManager.direction_for_language(lang)})
 
-    # All languages that are written right-to-left, according to https://lingohub.com/academy/best-practices/rtl-language-list
+
+
+
+class TranslateApiManager:
+    translate_client = None
+    translate_language_list = None
+
+    # Top 30 languages among EL in US k-12 schools, according to
+    # https://nces.ed.gov/programs/digest/d20/tables/dt20_204.27.asp
+    # As of 2021 Google Translate does not support Karen languages (kar) or Marshallese (mh), but should find the rest.
+    top_languages = ['es', 'ar', 'zh-CN', 'so', 'ru', 'pt', 'ht', 'hmn', 'ko', 'ur', 'fr', 'tg', 'sw', 'ja',
+                     'bn', 'hi', 'pa', 'my', 'fa', 'ne', 'kar', 'am', 'te', 'gu', 'km', 'uk', 'pl', 'mh']
+
+    # Languages that are written right-to-left, according to
+    # https://lingohub.com/academy/best-practices/rtl-language-list
     rtl_languages = ['ar', 'arc', 'dv', 'fa', 'ha', 'iw', 'khw', 'ks', 'ku', 'ps', 'ur', 'yi']
 
-    def direction_for_language(self, lang):
-        if lang in self.rtl_languages:
-            return 'rtl'
-        return 'ltr'
-
-
-class GoogleApiManager:
-    translate_client = None
+    @classmethod
+    def get_translate_language_list(cls):
+        if not cls.translate_language_list:
+            client = cls.get_google_translate_client()
+            if client:
+                cls.translate_language_list = []
+                for lang in client.get_languages():
+                    if lang['language'] in cls.top_languages:
+                        cls.translate_language_list.append(lang)
+                cls.translate_language_list.sort(key=lambda l: l['name'])
+        return cls.translate_language_list
 
     @classmethod
     def get_google_translate_client(cls):
@@ -61,3 +78,9 @@ class GoogleApiManager:
                 cls.translate_client = translate.Client.from_service_account_json(settings.GOOGLE_APPLICATION_CREDENTIALS)
                 logger.debug('Initialized new google translate client')
         return cls.translate_client
+
+    @classmethod
+    def direction_for_language(cls, lang):
+        if lang in cls.rtl_languages:
+            return 'rtl'
+        return 'ltr'
