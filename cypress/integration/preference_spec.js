cdad427977dd31f8aa97c2ef9207b518dc899fa6
Alan Harnum <waharnum@gmail.com>
2021-08-24 15:13:17 -0400

    CSL-1173: Utility functions for locating Reader iframe.

--- cypress/integration/preference_spec.js
+++ cypress/integration/preference_spec.js
@@ -1,5 +1,13 @@
 // Relies on user accounts created by `python manage.py createrostersamples`
 
+var readerIframeDocument = function () {
+    return cy.get('main#iframe-wrapper').find('iframe').its('0.contentDocument');
+}
+
+var readerIframeBody = function () {
+    return cy.get('main#iframe-wrapper').find('iframe').its('0.contentDocument').get('body');
+}
+
 describe('Student user samstudent...', () => {    
     
     it('Visits the home page and logs in', () => {
@@ -8,20 +16,23 @@ describe('Student user samstudent...', () => {
         cy.get('input#id_username').type('samstudent')
         cy.get('input#id_password').type('samstudent_pass')
         cy.get('button').contains('Log in').click()
-        cy.contains("Dive into the Library to find something to read.")
-      })     
+        cy.contains("Discover tools that make Clusive all your own.")
+        
+        
+            // cy.get('span.icon-settings').first().click()        
+            // cy.get('div#modalSettings').contains('Text size')
+            // cy.get('div#modalSettings').contains('Line spacing')
+            // cy.get('div#modalSettings').contains('Letter spacing')
+            // cy.get('div#modalSettings').contains('Font')
+            // cy.get('div#modalSettings').contains('Color')
+            // cy.get('div#modalSettings').contains('Word lookup links')
+            // cy.get('div#modalSettings').contains('Content navigation')        
+        
+        cy.visit('http://localhost:8000/reader/6/2')                                
+        
+        cy.wrap(readerIframeDocument).get('html').should('exist')
+        cy.wrap(readerIframeBody).should('exist')
 
-    it('Opens the settings panel', () => {
-        cy.get('span.icon-settings').first().click()
-        cy.get('div#modalSettings').contains('Display')
-        .contains('Reading Tools')
-        .contains('Text size')
-        .contains('Line spacing')
-        .contains('Letter spacing')
-        .contains('Font')
-        .contains('Color')
-        .contains('Word lookup links')
-        .contains('Content navigation')
-    })        
+      })     
   })
   
