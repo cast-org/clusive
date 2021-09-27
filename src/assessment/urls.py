c838d0203a10b3c90e94f939eba54e14ed22b132
Boris Goldowsky <bgoldowsky@cast.org>
2021-09-27 15:49:16 -0400

    CSL-1085: show comp check results on dashboard. Add 'createassessmentsamples' management command. Add a few more sample users to 'createrostersamples'. Add view for AJAX loading detail modals.

--- src/assessment/urls.py
+++ src/assessment/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('affect_check/<int:book_id>', views.AffectCheckView.as_view(), name='affect_check_view'),
     path('affect_detail/<word>', views.AffectDetailView.as_view(), name='affect_detail_view'),
-    path('comprehension_check/<int:book_id>', views.ComprehensionCheckView.as_view(), name='comprehension_check_view')
+    path('comprehension_check/<int:book_id>', views.ComprehensionCheckView.as_view(), name='comprehension_check_view'),
+    path('comprehension_detail/<int:book_id>', views.ComprehensionDetailView.as_view(), name='comprehension_detail_view'),
 ]
