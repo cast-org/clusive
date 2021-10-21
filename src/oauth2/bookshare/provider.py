ec31c49c1fabb4edb3ece06cc459aa3b923b890d
Joseph Scheuhammer <clown@alum.mit.edu>
2021-10-21 10:10:36 -0400

    CSL-1308: Fix documentation comments

--- src/oauth2/bookshare/provider.py
+++ src/oauth2/bookshare/provider.py
@@ -12,7 +12,7 @@ from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
 # the access token to get information about the Bookshare user.  What you can
 # get depends on Bookshare's API.  For example, there is a reference to a 
 # `userIdentifier` to use in requests, but need to determine how to reqeust this
-#  `userIdentifier` using the Bookshare API.
+# `userIdentifier` using the Bookshare API.
 # Note: initialize the base ProviderAccount:  ProviderAccount.__init__(self, social_account)
 class BookshareAccount(ProviderAccount):
     
@@ -25,13 +25,13 @@ class BookshareAccount(ProviderAccount):
 # - login url:                  get_login_url(self, request, kwargs)
 # -- this is the "local" server url that kicks off the SSO authorization
 # -- workflow that ends with receiving an access token from the provider.
-# -- for Google, it's Clusive's '/accounts/google/login'
-# -- and defined in a set of urls.py files
+# -- for Bookshare, it's Clusive's '/accounts/bookshare/login'
 #
 # - authorization parameters:    get_auth_params(self, request, action)
 # -- gets the AUTH_PARAMS from settings.py for this provider and other optional
 # -- params given in the current GET request's 'auth_params' entry.  It combines
 # -- the two and returns the combination of authorization parameters.
+# -- unclear if this is needed by Bookshare implementation
 #
 # - scope of authorization:      get_sope(self, request)
 # -- get the SCOPE from the settings.py for this provider and other optional
--- src/oauth2/bookshare/views.py
+++ src/oauth2/bookshare/views.py
@@ -11,16 +11,17 @@ class BookshareOAuth2Adapter(OAuth2Adapter):
     provider_id = BookshareProvider.id
     access_token_url = 'https://auth.bookshare.org/oauth/token'
     authorize_url = 'https://auth.bookshare.org/oauth/authorize'
-    # need to determine what is available from Bookshare in terms of user info
+    # Need to determine what is available from Bookshare in terms of user info
+    # In fact, this may not be an URL at all, but just an identfier.
     profile_url = ''
 
     # This is run after completing the OAuth2 workflow where Clusive has a
-    # fresh access token and can use it to request the user's profile.  It then
-    # stores the user info in the associated SocialAccount.
-    # TODO: find out if Bookshare supports a GET or POST for the user 
-    # information.
-    # Note: this is basically a copy of the GoogleOAuth2Adapter implementation,
-    # and is just a placeholder for now.
+    # fresh access token and can use it to request Bookshare user information.
+    # It then said user info in the associated SocialAccount.
+    # TODO: find out if Bookshare supports GET or POST for the user information
+    # It's possilble that allauth only implements GET.
+    # Note: the following is basically a copy of the GoogleOAuth2Adapter
+    # implementation and is just a placeholder for now.
     def complete_login(self, request, app, token, **kwargs):
         resp = requests.get(
             self.profile_url,
