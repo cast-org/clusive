81c8bada34e09745b6db27fe6fdd05862309df25
Joseph Scheuhammer <clown@alum.mit.edu>
2021-10-22 17:47:20 -0400

    CSL-1308: Complete oauth2 workflow up to getting the access token
    Still needed:
    - request user information from  bookshare and implement an
    `extract_uid()` method to handle it in the BookshareProvider class,
    - store the user information and access token in the database.

--- src/oauth2/bookshare/views.py
+++ src/oauth2/bookshare/views.py
@@ -1,34 +1,44 @@
 import requests
+from urllib.parse import urlencode
 
 from allauth.socialaccount.providers.oauth2.views import (OAuth2Adapter,
                                                           OAuth2LoginView,
                                                           OAuth2CallbackView)
+
 from allauth.socialaccount.providers.oauth2.client import OAuth2Error
 
 from .provider import BookshareProvider
 
+import pdb
+
 class BookshareOAuth2Adapter(OAuth2Adapter):
     provider_id = BookshareProvider.id
-    access_token_url = 'https://auth.bookshare.org/oauth/token'
     authorize_url = 'https://auth.bookshare.org/oauth/authorize'
+    access_token_url = 'https://auth.bookshare.org/oauth/token'
+    basic_auth = True   # Use basic Authorization in access token request
     # Need to determine what is available from Bookshare in terms of user info
-    # In fact, this may not be an URL at all, but just an identfier.
-    profile_url = ''
+    # Also, making the request to bookshare for the user info will.
+    # For testing, using the example of getting a list of books from Bookshare
+    # using an access token -- unfortunately, that returns a whole html page
+    # and not JSON (the line `extra_data = resp.json()` will fail)
+    profile_url = 'https://auth.bookshare.org/v2/mylists'
 
-    # This is run after completing the OAuth2 workflow where Clusive has a
-    # fresh access token and can use it to request Bookshare user information.
-    # It then said user info in the associated SocialAccount.
-    # TODO: find out if Bookshare supports GET or POST for the user information
-    # It's possilble that allauth only implements GET.
-    # Note: the following is basically a copy of the GoogleOAuth2Adapter
-    # implementation and is just a placeholder for now.
     def complete_login(self, request, app, token, **kwargs):
+        pdb.set_trace()
+        api_key = urlencode({
+            'api_key': app.client_id
+        })
+        print('BOOKSHAREOAUTH2ADAPTER.COMPLETE_LOGIN(): ', self.profile_url + '?' + api_key)
         resp = requests.get(
-            self.profile_url,
+            self.profile_url + '?' + api_key,
             params={"access_token": token.token, "alt": "json"},
         )
         resp.raise_for_status()
-        extra_data = resp.json()
+        #extra_data = resp.json()
+        extra_data = { 'status_code': resp.status_code } # for testing
+
+        # This current fails with "The provider must implement the extract_uid()
+        # method".  That is, add an extract_uid() to BookshareProvider class.
         login = self.get_provider().sociallogin_from_response(request, extra_data)
         print('BookshareOAuth2Adapter: %s', login)
         
@@ -36,4 +46,3 @@ class BookshareOAuth2Adapter(OAuth2Adapter):
 
 oauth2_login = OAuth2LoginView.adapter_view(BookshareOAuth2Adapter)
 oauth2_callback = OAuth2CallbackView.adapter_view(BookshareOAuth2Adapter)
-
