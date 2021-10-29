8915ebbd2aa6a47c8becdffbaf58cdbe7a3a5366
Joseph Scheuhammer <clown@alum.mit.edu>
2021-10-29 15:09:18 -0400

    CSL-1308: Remap 'socialaccount_connection' named url pattern to Clusive's reader page
    Remove references to allauth's 'connections/' endpoint and the allauth
    urls, views, and pages it imports and remap to a Clusive endpoint.

--- src/oauth2/bookshare/urls.py
+++ src/oauth2/bookshare/urls.py
@@ -1,21 +1,15 @@
 from django.urls import include, path
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
-from allauth.socialaccount import views as socialaccount_views
-import allauth
+from pages.views import ReaderIndexView
+#from allauth.socialaccount import views as socialaccount_views
+#import allauth
 
 from .provider import BookshareProvider
 
 urlpatterns = default_urlpatterns(BookshareProvider) + [
-    # Need the connection handlers to support multiple logins
-    path('connections/', socialaccount_views.connections, name="socialaccount_connections"),
-
-    # The templates for the following are automatically included by the
-    # `connections` views above.  The urls are included here so that when
-    # the connection view is called, there won't be a, e.g., "Reverse for
-    # 'account_email' not found ..." exception.  Ultimately, we want to get rid
-    # of these urls and the connections page itself since it includes
-    # extraneous signup, login, logout, reset password, and email endpoints.
-    # And, the OAuth2 workflow with Bookshare ends up back at allauth's
-    # connections page, not a Clusive page.
-    path('', include('allauth.account.urls'))
+    # Map 'socialaccount_connections' to Clusive's reader page.  This is because
+    # allauth hard codes a reverse('socialaccount_connections') to handle the
+    # end of the workflow triggered by '.../login?process=connect'.  By adding
+    # this path, the end of that workflow will end up on Clusive's reader page.
+    path('reader', ReaderIndexView.as_view(), name='socialaccount_connections')
 ]
