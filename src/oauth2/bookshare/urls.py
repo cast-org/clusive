926cb81fdf0722f32bdfd079da5bc20d2ca764ee
Joseph Scheuhammer <clown@alum.mit.edu>
2021-11-25 16:30:05 -0500

    CSL-1308: Remove override of DefaultSocialAccountAdapter to handle allauth's reverse of 'socialaccount_connections'.

--- src/oauth2/bookshare/urls.py
+++ src/oauth2/bookshare/urls.py
@@ -1,4 +1,12 @@
+from django.urls import path
 from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
+from pages.views import ReaderIndexView
 from .provider import BookshareProvider
 
-urlpatterns = default_urlpatterns(BookshareProvider)
+urlpatterns = default_urlpatterns(BookshareProvider) + [
+    # Map 'socialaccount_connections' to Clusive's reader page.  This is because
+    # allauth hard codes a reverse('socialaccount_connections') to handle the
+    # end of the workflow triggered by '.../login?process=connect'.  By adding
+    # this path, the end of that workflow will end up on Clusive's reader page.
+    path('reader', ReaderIndexView.as_view(), name='socialaccount_connections')
+]
