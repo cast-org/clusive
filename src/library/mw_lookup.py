b5c981e15235fd4535e673b93e67bdf4493c1644
Lynn McCormack <lmccormack@cast.org>
2022-01-25 14:35:37 -0500

    CSL-1521 - Merriam Webster Dictionary integration interim checkin - basic json to field mapping working - additional formatting cues and fields (e.g., quotes) need more test data

--- src/library/mw_lookup.py
+++ src/library/mw_lookup.py
@@ -4,23 +4,19 @@ import requests
 import json
 import re
 
+
 def json_extract(obj, key):
     # Recursively fetch values from nested JSON.
     arr = []
-
     def extract(obj, arr, key):
         # Recursively search for values of key in JSON tree.
-        #print('FINDING VALUES for key =' + key)
         if isinstance(obj, dict):
             for k, v in obj.items():
-                #print ('KEY = ' + k )
                 if k == key:
-                    #print ('FOUND A DT MATCH')
+                    #print('FOUND A DT MATCH')
                     arr.append(v)
                 elif isinstance(v, (dict, list)):
                     extract(v, arr, key)
-                #elif k == key:
-                #    arr.append(v)
         elif isinstance(obj, list):
             for item in obj:
                 extract(item, arr, key)
@@ -29,8 +25,40 @@ def json_extract(obj, key):
     values = extract(obj, arr, key)
     return values
 
+def clean_string(some_text):
+    clean_text = some_text
 
-# url = 'https://www.dictionaryapi.com/api/v3/references/sd3/json/' + word + '?key=' + dict_key
+    # delete any leading {bc}
+    clean_text = re.sub("^{bc}", "", clean_text)
+    # any internal {bc} gets replaced with :
+    clean_text = re.sub("{bc}", ": ", clean_text)
+
+    # remove link markings begin
+    clean_text = re.sub("{sx\|", "", clean_text)
+    # remove link markings end
+    clean_text = re.sub("\|\|\.*}", "", clean_text)
+
+    # remove italics markings - TODO replace w/html
+    clean_text = re.sub("{it}", "", clean_text)
+    clean_text = re.sub("{/it}", "", clean_text)
+
+    return clean_text
+
+def extract_examples(examples):
+    # return a list of strings
+    clean_examples = list()
+
+    # examples are dictionaries - extract the key 't'
+    for example in examples:
+        if example['t']:
+            clean_example = clean_string(example['t'])
+            clean_examples.append(clean_example)
+
+    return clean_examples
+
+
+
+# API_URL = 'https://www.dictionaryapi.com/api/v3/references/sd3/json/' + word + '?key=' + dict_key
 # r = requests.get(url)
 
 # json = r.json()
@@ -41,10 +69,8 @@ def json_extract(obj, key):
 # Opening JSON file
 f = open('definition.json')
 
-# returns JSON object as
-# a dictionary
+# returns JSON object as a dictionary
 data = json.load(f)
-#print("the length of the json = ", len(data))
 
 headwords = ["above", "alliteration", "boat", "career", "colored", "feline", "haze", "heart",
              "master", "Mauretania", "mistress", "plantation", "quotes", "savage",
@@ -52,77 +78,85 @@ headwords = ["above", "alliteration", "boat", "career", "colored", "feline", "ha
 
 # may want to exclude offensive words? not sure if there are any offensive words in intermediate dictionary
 
-for definition in data:
+for definition_entry in data:
     # clean any syllable markers (asterisk) from headword
     # remove anything inside curly brackets?
     #   example is N-allylnormorphine "hw":"{bit}N{\/bit}-al*lyl*nor*mor*phine"
-    clean_headword = definition["hwi"]["hw"].replace("*","")
+    clean_headword = ""
+    if definition_entry["hwi"]["hw"]:
+        clean_headword = definition_entry["hwi"]["hw"].replace("*", "")
+    else:
+        print("NO HEADWORD FOUND")
 
     # change this to match with currently selected word
     # need additional cleaning? compare only lower case?
     if clean_headword in headwords:
-        print('HEAD WORD = ' + (definition["hwi"]["hw"]).replace("*",""))
+        print('HEAD WORD = ' + clean_headword)
 
-        if definition["meta"]["stems"]:
-            print('STEMS or ALTERNATE FORMS =', definition["meta"]["stems"])
+        if definition_entry["meta"]["stems"]:
+            print('STEMS or ALTERNATE FORMS =', definition_entry["meta"]["stems"])
 
-        if 'fl' in definition:
-            print('POS = ' + definition["fl"])
+        if 'fl' in definition_entry:
+            print('POS = ' + definition_entry["fl"])
 
+        # COMMENTING OUT FOR NOW - URLS ARE DENIED
         # check for artwork
         # returns the art and a caption - no alt text
         # https://www.merriam-webster.com/assets/mw/static/art/dict/heart.gif
         # is there an assumption that there is only one image??
-        if 'art' in definition:
-            if definition["art"]["artid"]:
-                print('ART FILE: ', definition['art']['artid'])
-            if definition['art']['artid']:
-                print('ART CAPTION: ', definition['art']['capt'])
+        # MW_ART_URL = 'https://www.merriam-webster.com/assets/mw/static/art/dict/'
+        #
+        # if 'art' in definition_entry:
+        #     if definition_entry["art"]["artid"]:
+        #         art_url = MW_ART_URL + definition_entry['art']['artid']
+        #         print('ART FILE URL: ', art_url)
+        #     if definition_entry['art']['artid']:
+        #         art_caption = clean_string(definition_entry['art']['capt'])
+        #         # remove any sup in caption - TODO not sure about numbers - LDM
+        #         art_caption = re.sub("{sup}.*{/sup}", "", art_caption)
+        #         print('ART CAPTION: ', art_caption)
 
-        #print('short definition = ', definition["shortdef"])
+
+
+#print('short definition = ', definition["shortdef"])
 
         print('DEFINITIONS')
-        json_dt = json_extract(definition, 'dt')
-        #print(type(json_dt))
-        #print(json_dt)
-        #print("count of json_dt ", len(json_dt))
+        # extract a list of meanings with examples
+        json_dt = json_extract(definition_entry, 'dt')
+        print(json_dt);
 
+        #print("UNIQUE Definitions for this POS = ", len(json_dt))
         for def_content in json_dt:
-            print("DT OBJECT:", def_content)
-            #print("count of def_content = ", len(def_content))
-            # some content needs to be processed before looping through
-            # e.g., 'g' needs to be concat into a single string
-            #   sx should also be process - though not sure how
-            # loop through the content
-            for segments in def_content:
-                #print("SEGMENTS = ", segments)
-                index = 0
-                for segment in segments:
-                    #print("SEGMENT = ", segment)
-                    #print(type(segment))
-                    #print(segment[0:5])
-                    if segment == 'text':
-                        if segment[index + 1]:
-                            print("DEFINITION: ", segments[index+1])
-                    elif segment == 'vis':
-                        if segment[index + 1]:
-                            print("EXAMPLES = ", segments[index+1])
-                    index +=1
-            print("--")
 
-        #vis = json.load(def_content)
-            #vis = json_extract(def_content, 'vis')
-            #print("vis = ", vis)
+            clean_def = ""
 
-        # this does not work because the examples are no longer tied to the definition
-        #temp_vis = json.dumps(json_extract(json_dt, 't'))
-        #print(type(temp_vis))
-        ##print(temp_vis)
+            # Loop through the def_content: a list of name/value string pairs
+            for def_instance in def_content:
+                #print(def_instance)
+                if len(def_instance) > 1 and def_instance[1] is not None:
+                    # if this is a text check to see if there are any other parts to concat
+                    if def_instance[0] == 'text':
+                        clean_def = clean_def + clean_string(def_instance[1])
+                    # if this is a vis (example) then parse and clean the list of examples
+                    elif def_instance[0] == 'vis':
+                        #print("FOUND EXAMPLES: ", def_instance[1])
+                        examples = extract_examples(def_instance[1])
+                        print("CLEANED EXAMPLES = ", examples)
+                    # if this is an uns (unused) definition - discard
+                    elif def_instance[0] == 'uns':
+                        print("FOUND uns: ", def_instance[1])
+                    # if this is a g then concat with the text
+                    elif def_instance[0] == 'g':
+                        clean_def = clean_def + def_instance[1]
+                    # if this is another tag not accounted for - print to log file
+                    else:
+                        print("DEFINITION TYPE NOT FOUND = ", def_instance)
+            if clean_def != "":
+                print("CLEANED DEF: ", clean_def)
 
         print()
         print()
 
-# Closing file
 f.close()
 
 
