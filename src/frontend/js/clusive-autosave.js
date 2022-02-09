5c12a91465127e5d99e00e6fe8af41aa681bc239
Boris Goldowsky <bgoldowsky@cast.org>
2022-02-09 12:50:40 -0500

    CSL-1556: Basics of migration to v2.  (2.0.0-beta.10) Create variable for R2 instance, use it instead of D2Reader static. Make recommended HTML changes. Also enabled LineFocus and pasted in needed CSS (which should be moved to a better place).

--- src/frontend/js/clusive-autosave.js
+++ src/frontend/js/clusive-autosave.js
@@ -1,74 +1,74 @@
 /* Code for comprehension and affect assessments */
-/* global clusive, clusiveContext, PAGE_EVENT_ID, DJANGO_CSRF_TOKEN, fluid, D2Reader */
+/* global clusive, clusiveContext, PAGE_EVENT_ID, DJANGO_CSRF_TOKEN, fluid */
 /* exported clusiveAutosave */
 
 var clusiveAutosave = {
     messageQueue: clusive.djangoMessageQueue({
-            config: {                        
+            config: {
                 localStorageKey: "clusive.messageQueue.autosave",
                 lastQueueFlushInfoKey: "clusive.messageQueue.autosave.log.lastQueueFlushInfo"
             }
         }),
-    // Test if data is equivalent for autosave purposes        
-    isEquivalentData: function (oldData, newData) {                
+    // Test if data is equivalent for autosave purposes
+    isEquivalentData: function (oldData, newData) {
         if(!oldData || !newData) {
             return false;
         }
         var isEquivalent = true;
 
         Object.keys(newData).forEach(function (key) {
-            if(newData[key] !== oldData[key]) {                
-                isEquivalent = false;                
+            if(newData[key] !== oldData[key]) {
+                isEquivalent = false;
             }
-        }); 
+        });
 
-        return isEquivalent;                
+        return isEquivalent;
     },
-    save: function(url, data) {        
-        var lastData = clusiveAutosave.lastDataCache[url];        
-        var isNewData = !clusiveAutosave.isEquivalentData(lastData, data);                
+    save: function(url, data) {
+        var lastData = clusiveAutosave.lastDataCache[url];
+        var isNewData = !clusiveAutosave.isEquivalentData(lastData, data);
         if(isNewData) {
             console.debug("adding changed data for URL " + url + " to autosave queue: ", data);
-            
+
             // Test if we should replace an existing autosave message
             // TODO: this functionality should be ported over to the message queue,
             // should the need for it arise outside the autosave
 
-            var currentPosition = clusiveAutosave.messageQueue.getMessages().findIndex(function (message) {                
-                if(message.content.type === "AS" && message.content.url === url) {                    
+            var currentPosition = clusiveAutosave.messageQueue.getMessages().findIndex(function (message) {
+                if(message.content.type === "AS" && message.content.url === url) {
                     return true;
-                }                
+                }
                 return false;
             });
-            if(currentPosition >= 0) {                
+            if(currentPosition >= 0) {
                 var newMessage = clusiveAutosave.messageQueue.wrapMessage({"type": "AS", "url": url, "data": JSON.stringify(data)});
                 clusiveAutosave.messageQueue.queue[currentPosition] = newMessage;
                 clusiveAutosave.messageQueue.syncToLocalStorage();
             }
             else {
                 clusiveAutosave.messageQueue.add({"type": "AS", "url": url, "data": JSON.stringify(data)});
-            }            
-            clusiveAutosave.lastDataCache[url] = data; 
+            }
+            clusiveAutosave.lastDataCache[url] = data;
         } else {
             console.debug("no changed data for URL " + url + ", not adding to autosave queue", data)
         }
     },
-    retrieve: 
-        function(url, callback) {                    
-            var autosaveMessages = [].concat(clusiveAutosave.messageQueue.getMessages()).filter(function (item) {                    
+    retrieve:
+        function(url, callback) {
+            var autosaveMessages = [].concat(clusiveAutosave.messageQueue.getMessages()).filter(function (item) {
                     if(item.content.type === "AS" && item.content.url === url) {
                         return true;
-                    }                    
+                    }
             });
-            
-            if(autosaveMessages.length > 0) {                                        
-                var latestLocalData = JSON.parse(autosaveMessages.pop().content.data);                
+
+            if(autosaveMessages.length > 0) {
+                var latestLocalData = JSON.parse(autosaveMessages.pop().content.data);
                 callback(latestLocalData);
                 clusiveAutosave.lastDataCache[url] = latestLocalData;
-            } else {                   
+            } else {
                 console.debug("No local data for url: " + url + ", trying to get from server");
-                $.get(url, function(data) {                    
-                    callback(data);                    
+                $.get(url, function(data) {
+                    callback(data);
                     clusiveAutosave.lastDataCache[url] = data;
                 }).fail(function(error) {
                     if (error.status === 404) {
@@ -77,7 +77,7 @@ var clusiveAutosave = {
                         console.warn('failed to get data: ', error.status);
                     }
                 });
-            }                 
+            }
         },
     // Maintains per-url record of data to avoid unneeded autosaving
     lastDataCache: {
