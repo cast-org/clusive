fd577f49357f1aa6fc5b9437441edd7341991029
Boris Goldowsky <bgoldowsky@cast.org>
2022-08-15 12:57:04 -0400

    CSL-1880: Document how to add teacher resources.

--- local_install.ps1
+++ local_install.ps1
@@ -9,5 +9,6 @@ cd target
 python manage.py migrate
 python manage.py loaddata preferencesets tiptypes callstoaction subjects
 python manage.py importdir ../content
+python manage.py import_resources ../resources/resources.json
 echo "Setup complete! Run python manage.py createsuperuser to create the super user and you're all done"
 cd ..
