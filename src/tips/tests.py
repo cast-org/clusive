b0f21961e8a20b9d83e7114b2d6811b78d91f7fc
Joseph Scheuhammer <clown@alum.mit.edu>
2022-10-27 17:47:56 -0400

    CSL-2040: Add test case for `switch` when there is only one version of the book

--- src/tips/tests.py
+++ src/tips/tests.py
@@ -97,9 +97,13 @@ class TipTypeTestCase(TestCase):
         else:
             return False
 
-    # TipType Method tested:
-    # def can_show(self, page: str, version_count: int, user: ClusiveUser):
     def can_show_tips(self, clusive_user):
+        """
+        Tests all tip types on all pages for the given user.
+            - testing TipType.can_show(page, version_count, user).
+            - note that only version counts greater than one are tested here.
+            - TipType.can_show(page: str, version_count: int, user: ClusiveUser)
+        """
         version_count = 3
         for page_name in self.page_names:
             for tip in TipType.objects.all():
@@ -107,9 +111,26 @@ class TipTypeTestCase(TestCase):
                 expected = self.look_up_expected(clusive_user, page_name, tip)
                 self.assertEqual(
                     actual, expected,
-                    f"Can show {tip.name} for {clusive_user.user.username} on {page_name}"
+                    f"Can show {tip.name} tip for {clusive_user.user.username} on {page_name} page"
                 )
 
     def test_can_show(self):
+        """
+        Test all users for all tips on all pages with a version count greater
+        than 1 (one).
+        """
         for clusive_user in ClusiveUser.objects.all():
             self.can_show_tips(clusive_user)
+
+    def test_can_show_switch_single_version(self):
+        """
+        Special case: Test all users for the `switch` tip with only one book
+        version and for the 'Reading' page only.  It should always be `False`.
+        """
+        for clusive_user in ClusiveUser.objects.all():
+            switch_tip = TipType.objects.get(name='switch')
+            actual = switch_tip.can_show('Reading', 1, clusive_user)
+            self.assertFalse(
+                actual,
+                f"Can show 'switch' tip for {clusive_user.user.username} on Reading page"
+            )
