b56fe5a6cc6142fb53eef3be8ff12794255de868
Cindy Qi Li <cli@ocadu.ca>
2022-11-11 13:13:25 -0500

    NOJIRA: address code review comments

--- src/tips/management/commands/reset_tips_histories.py
+++ src/tips/management/commands/reset_tips_histories.py
@@ -1,7 +1,10 @@
+import logging
 from django.core.management.base import BaseCommand, CommandError
 
-from django.contrib.auth.models import User
+from roster.models import ClusiveUser
 from tips.models import TipHistory
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'In the tip history table, reset these field values:\n' \
@@ -17,27 +20,27 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         uname = options['username']
         if uname:
-            user = User.objects.filter(username=uname)
-            if user:
-                self.reset_one_user_tip_history(user[0].id, uname)
-            else:
+            try:
+                clusive_user = ClusiveUser.objects.get(user__username=uname)
+            except ClusiveUser.DoesNotExist:
                 raise CommandError('User name \'%s\' not found' % uname)
+            self.reset_one_user_tip_history(clusive_user.id, uname)
         else:
             self.reset_all_tip_histories()
 
-    def reset_a_tip_history(self, tip):
-        tip.show_count = 0
-        tip.last_attempt = None
-        tip.last_show = None
-        tip.last_action = None
-        tip.save()
+    def reset_a_tip_history(self, tip_history):
+        tip_history.show_count = 0
+        tip_history.last_attempt = None
+        tip_history.last_show = None
+        tip_history.last_action = None
+        tip_history.save()
 
     def reset_one_user_tip_history(self, uid, uname):
-        for tip in TipHistory.objects.filter(user_id=uid):
-            tip = self.reset_a_tip_history(tip)
-        self.stdout.write("Done: the tip history is reset for the user '" + uname + "'")
+        for tip_history in TipHistory.objects.filter(user_id=uid):
+            self.reset_a_tip_history(tip_history)
+        logger.info("Done: the tip history is reset for the user '" + uname + "'")
 
     def reset_all_tip_histories(self):
-        for tip in TipHistory.objects.all():
-            tip = self.reset_a_tip_history(tip)
-        self.stdout.write("Done: all tip histories are reset")
+        for tip_history in TipHistory.objects.all():
+            self.reset_a_tip_history(tip_history)
+        logger.info("Done: all tip histories are reset")
